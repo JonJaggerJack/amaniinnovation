0c2a851ba888f046d1f8b5f54b9ecffb4af8e287
vincejs77 <vinjobs77@gmail.com>
2022-08-25 09:19:45 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	// command/ npx tailwindcss -i ./src/assets/css/inputs_styles.css -o ./public/css/outputs_styles.css --minify --watch
+	content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				Roboto: ["Roboto", "sans-serif"],
+				Raleway: ["Raleway", "sans-serif"]
+			}
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			white: "#ffffff",
+			cwhite: "#FAFAFA",
+			cblack: "#023948",
+			cgray: {
+				100: "#60606013",
+				300: "#60606043",
+				800: "#60606097",
+				900: "#606060"
+			},
+			cgreen: "#66ACBF",
+			cyellow: "#F7DA33"
+		}
+	},
+	plugins: []
+};
