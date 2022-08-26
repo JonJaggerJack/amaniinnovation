19bd837fb84d2125bc0e733f64ae1c882793ab4f
vincejs77 <vinjobs77@gmail.com>
2022-08-26 14:55:01 +0100

    update 02 05

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="home bg-cblack bg-[url('bg/2.jpg')] bg-cover bg-center relative">
-    <div class="absolute top-0 right-0 w-full h-full bg-cblack opacity-70"></div>
-    <div class="max-w-7xl py-32 mx-auto text-left md:text-center px-4 lg:px-0">
-      <div class="relative max-w-4xl">
-        <h1
-          class="pb-2 mt-[250px] md:mt-[300px] text-3xl font-extrabold text-left text-transparent sm:text-3xl md:text-6xl md:text-left bg-clip-text bg-gradient-to-r from-cyellow via-cwhite to-cyellow"
-        >
-          Au-delà de vos limites
-        </h1>
+  <div class="home">
+    <section class="bg-cblack bg-[url('bg/2.jpg')] bg-cover bg-center relative">
+      <div class="absolute top-0 right-0 w-full h-full bg-cblack opacity-70"></div>
+      <div class="max-w-7xl py-20 md:py-32 mx-auto text-left md:text-center px-4 xl:px-0">
+        <div class="relative max-w-4xl">
+          <h1
+            class="pb-2 mt-[300px] md:mt-[300px] text-3xl font-extrabold text-left text-transparent sm:text-3xl md:text-6xl md:text-left bg-clip-text bg-gradient-to-r from-cyellow via-cwhite to-cyellow"
+          >
+            Au-delà de vos limites
+          </h1>
 
-        <p
-          class="mt-8 text-sm text-left text-cwhite sm:text-base md:text-lg md:max-w-xl md:text-left"
-        >
-          Société née des initiatives pour l’émergence et la croissance dans le domaine de
-          l’eau, énergie, transport, agroalimentaire en général et immobilier au sein de
-          la ville de Bukavu.
-        </p>
+          <p
+            class="mt-8 text-sm text-left text-cwhite sm:text-base md:text-lg md:max-w-xl md:text-left"
+          >
+            Société née des initiatives pour l’émergence et la croissance dans le domaine
+            de l’eau, énergie, transport, agroalimentaire en général et immobilier au sein
+            de la ville de Bukavu.
+          </p>
 
-        <div class="mt-10">
-          <div class="">
-            <button
-              class="transform hover:scale-[0.97] flex w-auto justify-start btn group whitespace-nowrap items-center text-base font-bold leading-5 text-left text-cblack bg-cyellow rounded-md md:text-md py-2 px-6 md:text-center"
-            >
-              <router-link to="" class="flex justify-start items-center">
-                En savoir plus
+          <div class="mt-10">
+            <div class="">
+              <button
+                class="transform hover:scale-[0.97] flex w-auto justify-start btn group whitespace-nowrap items-center text-base font-bold leading-5 text-left text-cblack bg-cyellow rounded-md md:text-md py-2 px-6 md:text-center"
+              >
+                <router-link to="" class="flex justify-start items-center">
+                  En savoir plus
 
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-4 h-4 ml-2 opacity-50 group-hover:opacity-95"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                  />
-                </svg>
-              </router-link>
-            </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-4 h-4 ml-2 opacity-50 group-hover:opacity-95"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                    />
+                  </svg>
+                </router-link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+    <HomeS1 />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HomeS1 from "./sections/HomeS1.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    HomeS1,
+  },
 };
 </script>
---
+++ src/views/sections/HomeS1.vue
@@ -0,0 +1,94 @@
+<template>
+  <!-- Section 1 -->
+  <section class="w-full bg-white">
+    <div
+      class="flex flex-col px-4 xl:px-0 py-20 mx-auto max-w-7xl lg:flex-row lg:space-x-24"
+    >
+      <div
+        class="relative w-full mb-10 lg:text-left sm:text-center lg:w-1/2 xl:w-7/12 lg:mb-0"
+      >
+        <p class="text-sm font-semibold tracking-wide uppercase text-cgreen">
+          Objectif & vision
+        </p>
+        <h2 class="text-4xl font-black sm:text-5xl mt-7 sm:leading-tight">
+          Oeuvrer dans des secteurs à impact rapide et intégrateurs
+        </h2>
+        <p class="text-lg lg:max-w-2xl mt-9 mb-8">
+          Prenant conscience du chômage élevé exacerbant le niveau de pauvreté en Afrique
+          et épris par le souci de développement et de faire mieux :
+        </p>
+        <ul class="relative max-w-2xl mx-auto lg:mx-0">
+          <li class="flex pl-10 sm:pl-12 mt-5">
+            <svg
+              class="absolute left-0 w-6 h-6 sm:h-8 sm:w-8 text-cgreen"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="text-md text-gray-600"
+              ><span class="font-bold text-gray-900">Amani Innovation </span> a pour
+              vision d’œuvrer dans des secteurs à impact rapide et intégrateurs, pouvant
+              améliorer l’écosystème entrepreneurial et l’environnement
+              socioéconomique</span
+            >
+          </li>
+
+          <li class="flex pl-10 sm:pl-12 mt-5">
+            <svg
+              class="absolute left-0 w-6 h-6 sm:h-8 sm:w-8 text-cgreen"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="text-md text-gray-600"
+              ><span class="font-bold text-gray-900">En particulier</span> au sein de la
+              ville de Bukavu et de ses environs ,de toute la RDC , dans la région du
+              grand lacs et en général dans toute</span
+            >
+          </li>
+        </ul>
+
+        <div>
+          <button class="transform hover:scale-[0.97]">
+            <router-link
+              to="#_"
+              class="inline-block px-5 py-3 mt-8 text-sm text-white bg-cblack rounded-md hover:bg-gray-800"
+            >
+              <span class="font-medium">Decouvrez nos services</span> -
+              <span class="font-light">ici</span>
+            </router-link>
+          </button>
+        </div>
+      </div>
+
+      <div
+        class="relative flex flex-col items-center justify-center flex-shrink-0 w-full max-w-2xl py-10 sm:py-16 mx-auto bg-cgray-100 rounded-lg lg:w-1/2 lg:mx-0 xl:w-5/12"
+      >
+        <img
+          src="/assets/img/content/1.png"
+          alt="Amani Innovation"
+          class="w-[200px] sm:w-[300px]"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomeS1",
+  setup() {},
+};
+</script>
