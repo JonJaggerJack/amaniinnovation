eaf4cae538123028c2c8faae011a35bad6e579bf
vincejs77 <vinjobs77@gmail.com>
2022-08-28 11:32:08 +0100

    update 04 07

--- src/views/sections/AboutS2.vue
+++ src/views/sections/AboutS2.vue
@@ -8,7 +8,10 @@
       <div
         class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10"
       >
-        <img src="/assets/img/content/1_0.svg" class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20" />
+        <img
+          src="/assets/img/content/1_0.svg"
+          class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 w-[300px] sm:w-[420px] mx-auto"
+        />
       </div>
 
       <!-- Content -->
@@ -23,37 +26,43 @@
         <p
           class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 text-md lg:text-lg"
         >
-          Build an atmosphere that creates productivity in your organization and your
-          company culture.
+          Est d’œuvrer dans des secteurs à impact rapide et intégrateurs, pouvant
+          améliorer :
         </p>
         <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Maximize productivity and growth
+            <span> L’écosystème entrepreneurial </span>
           </li>
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Speed past your competition
+            <span> Et l’environnement socioéconomique </span>
           </li>
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Learn the top techniques
+            <span> En particulier au sein de la ville de Bukavu </span>
           </li>
         </ul>
       </div>
       <!-- End  Content -->
     </div>
     <div
-      class="box-border flex flex-col items-center content-center px-4 mx-auto mt-16 leading-6 text-black border-0 border-gray-300 border-solid md:mt-16 md:flex-row max-w-6xl lg:px-4"
+      class="box-border flex flex-col items-center content-center px-4 mx-auto mt-8 leading-6 text-black border-0 border-gray-300 border-solid md:mt-16 md:flex-row max-w-6xl lg:px-4"
     >
       <!-- Content -->
       <div class="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
@@ -65,30 +74,37 @@
         <p
           class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-10 lg:text-lg"
         >
-          Save time and money with our revolutionary services. We are the leaders in the
-          industry.
+          En vue d'œuvrer par package de produit en débutant par la transformation
+          industrielle au travers la production et l’offre de l’eau de source au sein de
+          la ville de Bukavu. Amani Innovation se consolide par :
         </p>
         <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Automated task management workflow
+            <span> Une relation basée sur la confiance </span>
           </li>
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Detailed analytics for your data
+            <span> La solidarité entre les actionnaire </span>
           </li>
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Some awesome integrations
+            <span> La fibre sociale du lien familial qui les unit </span>
           </li>
         </ul>
       </div>
@@ -98,7 +114,10 @@
       <div
         class="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2"
       >
-        <img src="/assets/img/content/1_2.svg" class="pl-4 sm:pr-10 xl:pl-10 lg:pr-32" />
+        <img
+          src="/assets/img/content/1_2.svg"
+          class="pl-0 sm:pr-10 xl:pl-10 lg:pr-32 w-[180px] sm:w-[430px] mx-auto"
+        />
       </div>
     </div>
 
@@ -109,7 +128,10 @@
       <div
         class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10"
       >
-        <img src="/assets/img/content/1_1.svg" class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20" />
+        <img
+          src="/assets/img/content/1_1.svg"
+          class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 w-[300px] sm:w-[450px] mx-auto"
+        />
       </div>
 
       <!-- Content -->
@@ -124,30 +146,36 @@
         <p
           class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg"
         >
-          Build an atmosphere that creates productivity in your organization and your
-          company culture.
+          Nous voulons créer des activités dans different secteurs, dont les plus envisagé
+          :
         </p>
         <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Maximize productivity and growth
+            <span> Transformation industrielle et courtage </span>
           </li>
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Speed past your competition
+            <span> Assurances et de transport et communication </span>
           </li>
-          <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+          <li
+            class="flex justify-start items-center box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
+          >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-cblack bg-cyellow rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-3 text-cblack bg-cyellow rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
-            Learn the top techniques
+            <span> Commerce général, import-export, logistique </span>
           </li>
         </ul>
       </div>
